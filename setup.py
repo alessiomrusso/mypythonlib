9277fc83445f0ec059fcb3a85a7a30f847bbc9f4
alessiomrusso <alessiomrusso@gmail.com>
2024-11-08 19:44:03 +0100

    build: configure semantic versioning

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages, setup
 
+__version__ = "0.0.0"
+
 setup(
     name='mypythonlib',
     packages=find_packages(include=['mypythonlib']),
-    version='0.1.0',
+    version=__version__,
     description='My first Python library',
     author='Me',
     install_requires=[],

5f92e32034ab28cea40585906dfa90f883356bfd
alessiomrusso <alessiomrusso@gmail.com>
2024-11-08 19:59:05 +0100

    feat: rename module
    BREAKING CHANGE: myfunctions was renamed to haversine

--- tests/test_haversine.py
+++ tests/test_haversine.py
@@ -1,7 +1,7 @@
-from mypythonlib import myfunctions
+from mypythonlib import haversine
 
 def test_haversine():
     # Amsterdam to Berlin
-    assert myfunctions.haversine(
+    assert haversine.haversine(
         4.895168, 52.370216, 13.404954, 52.520008
     ) == 576.6625818456291
